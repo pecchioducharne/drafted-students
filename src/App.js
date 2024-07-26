1681c798d2f43231b9a4d8818fecfc96b3e9fac1
pecchioducharne <pecchioducharne@gmail.com>
2024-07-26 12:24:44 -0600

    cleaning up, removing unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MultiStepForm from "./components/MultiStepForm";
-import VideoRecorderPage from "./components/VideoRecorderPage";
-import VideoRecorderPage2 from "./components/VideoRecorderPage2";
-import VideoRecorderPage3 from "./components/VideoRecorderPage3";
 import { UserProvider } from "./components/UserContext";
 
 const App = () => {
@@ -13,9 +10,6 @@ const App = () => {
         <div className="App">
           <Routes>
             <Route path="/" element={<MultiStepForm />} />
-            <Route path="/video-recorder" element={<VideoRecorderPage />} />
-            <Route path="/video-recorder2" element={<VideoRecorderPage2 />} />
-            <Route path="/video-recorder3" element={<VideoRecorderPage3 />} />
           </Routes>
         </div>
       </UserProvider>
